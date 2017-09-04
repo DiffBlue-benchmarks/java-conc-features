7ed183ae0515170b0edc91f64c6cce3ffb482a9c
Kurt Degiorgio <degiorgiokurt@outlook.com>
2017-09-04 08:25:46 +0100

    Crawler update

--- github_crawler/crawler.py
+++ github_crawler/crawler.py
@@ -1,11 +1,15 @@
+# TODO: problem  API is limited to 1000 repos per search query (https://developer.github.com/v3/search/#about-the-search-api)
+
+import pdb
 import requests
 import json
-from pprint import pprint
+import numpy as np
 
+fetching_rate=100
 url='https://api.github.com/graphql'
 token=""
 headers={'Authorization': 'token '+token}
-repo_search_query="language:Java stars:>6000"
+repo_search_query="language:Java stars:>1350"
 repo_search_graphql="""
 {{
   search(query: \"{0}\", type: REPOSITORY, first: {1}{2})  {{
@@ -38,6 +42,33 @@ repo_search_graphql="""
 }}
 """
 
+# methods to extract data from result
+def next_page(response):
+    try:
+        return response["data"]["search"]["pageInfo"]["hasNextPage"]
+    except Exception:
+        print_error(response)
+        raise
+def get_next_page_id(response):
+    try:
+        return response["data"]["search"]["pageInfo"]["endCursor"]
+    except Exception:
+        print_error(response)
+        raise
+def get_total_repos(response):
+    try:
+        return response["data"]["search"]["repositoryCount"]
+    except Exception:
+        print_error(response)
+        raise
+def get_edges(response):
+    try:
+        return response["data"]["search"]["edges"]
+    except Exception:
+        print_error(response)
+        raise
+
+# request data from github related methods.
 def build_repo_search_request(query, first, after):
     modified_after=""
     if after!="":
@@ -47,55 +78,61 @@ def do_request(query):
     r2=requests.post(url, json.dumps({"query": query}), headers=headers)
     r=r2.json()
     ajson=json.loads(json.dumps(r))
-#   pprint(json.dumps(r))
     return ajson;
-def next_page(response):
-    return response["data"]["search"]["pageInfo"]["hasNextPage"]
-def get_next_page_id(response):
-    return response["data"]["search"]["pageInfo"]["endCursor"]
-def get_total_repos(repose):
-    return response["data"]["search"]["repositoryCount"]
-def extract_repo(repo_dict, response):
+def extract_repo(response):
     number_extacted=0
-    for edges in response["data"]["search"]["edges"]:
-        number_extacted+=1
+    np_numeric_data=np.zeros(shape=(len(get_edges(response)),4))
+    repo_names=[]
+    for edges in get_edges(response):
         repo=edges['node']
         total_stars=repo['stargazers']['totalCount']
         total_watched=repo['watchers']['totalCount']
         total_forked=repo['forks']['totalCount']
         total_size=repo['diskUsage']
         name=repo['name']
-        significance=total_stars+(.75*(1-total_size))+(.5*(total_watched+total_forked))
-        repo_dict.append({"significance" : significance,  "stars" : total_stars, \
-                "watched" : total_watched, \
-                "forked" : total_forked, \
-                "size" : total_size,
-                "name" : name})
-    return number_extacted
+        repo_names.append(name)
+        np_numeric_data[number_extacted, :] = [total_stars, total_forked, total_size, total_watched]
+        number_extacted+=1
+    return number_extacted, repo_names, np_numeric_data
 
+# utility methods.
+def calculate_significance(a):
+    return a[0]+(.75*(1-a[2]))+(.5*(a[3]+a[1]))
+def print_error(response):
+    print "---->ERROR:"
+    print response
+    print "---->Exception:"
+
+# main loop
 current_page_id=""
 repo_dict = []
+np_repo_data=np.zeros(shape=(0, 4))
 while True:
-    request_str=build_repo_search_request(repo_search_query, 100, current_page_id)
+    request_str=build_repo_search_request(repo_search_query, fetching_rate, current_page_id)
     response=do_request(request_str)
     current_page_id=get_next_page_id(response)
-    number_extracted=extract_repo(repo_dict, response)
+    number_extracted, temp_repo_names, temp_repo_data=extract_repo(response)
+    np_repo_data=np.concatenate((np_repo_data, temp_repo_data), axis=0)
+    repo_dict=repo_dict+temp_repo_names
     print "fetched {0}. Progress: {1} out of {2}." \
         .format(number_extracted, len(repo_dict), get_total_repos(response))
     if not next_page(response):
         break
 
-newlist=sorted(repo_dict, key=lambda k: k['significance'], reverse=True)
-print("total number of projects extracted: {0}".format(len(repo_dict)))
-for repo_ex in newlist:
-    print repo_ex["name"],
-    print ",",
-    print repo_ex["significance"],
-    print ",",
-    print repo_ex["stars"],
-    print ",",
-    print repo_ex["forked"],
-    print ",",
-    print repo_ex["watched"],
-    print ",",
-    print repo_ex["size"]
+print "-------"
+
+#normalize and calculate significance.
+normalized_repo_data=np_repo_data/np_repo_data.max(axis=0)
+significance=np.apply_along_axis(calculate_significance, 1, normalized_repo_data)
+
+#create dictionary (there is more platonic ways of doing this but anyways...)
+repo_dict2={}
+index=0
+for repo in  repo_dict:
+    repo_dict2[repo]=significance[index]
+    index+=1
+
+#order by decreasing significance and print
+repo_dict_sorted=sorted(repo_dict2.items(), key=lambda x : x[1], reverse=True)
+for repo in repo_dict_sorted:
+    print repo[0]
